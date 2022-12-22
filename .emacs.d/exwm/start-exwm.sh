5f412194315509093b4c1f73e03595350ba3f73b
Gary Glover <gary_glover@next.co.uk>
2022-12-22 15:51:08 +0000

    Screen locking

--- .emacs.d/exwm/start-exwm.sh
+++ .emacs.d/exwm/start-exwm.sh
@@ -3,5 +3,8 @@
 # NOTE: This is only for the live demo, not needed for your configuration!
 # spice-vdagent
 
+# Screen locking suspend
+xss-lock -- slock &
+
 # Fire it up
 exec dbus-launch --exit-with-session emacs -mm --debug-init
