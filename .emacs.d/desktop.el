87de9bf47f325cd94619f702b2988df7e07d5f98
Gary Glover <light.bed5489@fourleafclover.uk>
2023-02-06 12:04:06 +0000

    Migrate window commands to s-j hydra

--- .emacs.d/desktop.el
+++ .emacs.d/desktop.el
@@ -1,5 +1,5 @@
-(defun efs/run-in-background (command)
-  (let ((command-parts (split-string command "[ ]+")))
+(defun efs/run-in-background (command) 
+  (let ((command-parts (split-string command "[ ]+"))) 
     (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
 
 (defun efs/exwm-init-hook ()
@@ -13,8 +13,8 @@
   (display-battery-mode 1)
 
   ;; Show the time and date in modeline
-  (setq display-time-day-and-date t)
-  (setq display-time-format "%a %d %b %H:%M")
+  (setq display-time-day-and-date t) 
+  (setq display-time-format "%a %d %b %H:%M") 
   (display-time-mode 1)
   ;; Also take a look at display-time-format and format-time-string
 
@@ -25,106 +25,108 @@
   ;;(efs/run-in-background "pasystray")
   )
 
-(defun clover/xmodmap ()
-  (if (string= (system-name) "clover-20f600a2uk")
-      (start-process-shell-command "xmodmap" nil "xmodmap ~/xmodmap/lenovo_xmodmap")))
+(defun clover/xmodmap () 
+  (cond ((string= (system-name) "clover-20f600a2uk") 
+	 (start-process-shell-command "xmodmap" nil "xmodmap ~/xmodmap/lenovo_xmodmap")) 
+	((string= (system-name) "clover-z270pd3") 
+	 (start-process-shell-command "xmodmap" nil "xmodmap ~/xmodmap/redragon_keyboard_xmodmap"))))
 
-(defun efs/exwm-update-class ()
-  (exwm-workspace-rename-buffer exwm-class-name))
+(defun efs/exwm-update-class () 
+        (exwm-workspace-rename-buffer exwm-class-name))
 
-(defun clover/exwm-update-title ()
-  (exwm-workspace-rename-buffer (concat exwm-class-name ": " exwm-title)))
+(defun clover/exwm-update-title () 
+        (exwm-workspace-rename-buffer (concat exwm-class-name ": " exwm-title)))
 
 (defun clover/exwm-lock-screen ()
-  (start-process-shell-command "lock" nil "slock"))
+        (interactive)
+        (start-process-shell-command "lock" nil "slock"))
 
-(use-package exwm
-  :config
-  ;; Set the default number of workspaces
-  (setq exwm-workspace-number 10)
+(use-package 
+        exwm 
+        :config
+        ;; Set the default number of workspaces
+        (setq exwm-workspace-number 10)
 
-  ;; When window "class" updates, use it to set the buffer name
-  ;; (add-hook 'exwm-update-class-hook #'efs/exwm-update-class)
-  (add-hook 'exwm-update-title-hook #'clover/exwm-update-title)
+        ;; When window "class" updates, use it to set the buffer name
+        ;; (add-hook 'exwm-update-class-hook #'efs/exwm-update-class)
+        (add-hook 'exwm-update-title-hook #'clover/exwm-update-title)
 
-  ;; When EXWM starts up, do some extra confifuration
-  (add-hook 'exwm-init-hook #'efs/exwm-init-hook)
+        ;; When EXWM starts up, do some extra confifuration
+        (add-hook 'exwm-init-hook #'efs/exwm-init-hook)
 
-  ;; Rebind CapsLock to Ctrl
-  ;; (start-process-shell-command "xmodmap" nil "xmodmap ~/.Xmodmap")
-  (clover/xmodmap)
+        ;; Rebind CapsLock to Ctrl
+        ;; (start-process-shell-command "xmodmap" nil "xmodmap ~/.Xmodmap")
+        (clover/xmodmap)
 
-  ;; Set the screen resolution (update this to be the correct resolution for your screen!)
-  (require 'exwm-randr)
-  (exwm-randr-enable)
-  (start-process-shell-command "xrandr" nil "xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 1920x1200 --pos 0x480 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1200 --pos 5760x480 --rotate normal --output DP-5 --off")
-  (setq exwm-randr-workspace-monitor-plist '(1 "DP-0" 2 "DP-0" 3 "DP-0" 7 "DP-4" 8 "DP-4" 9 "DP-4"))
-	;;exwm-workspace-warp-cursor t)
-	;;mouse-autoselect-window t)
+        ;; Set the screen resolution (update this to be the correct resolution for your screen!)
+        (require 'exwm-randr) 
+        (exwm-randr-enable) 
+        (start-process-shell-command "xrandr" nil
+                "xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 1920x1200 --pos 0x480 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1200 --pos 5760x480 --rotate normal --output DP-5 --off") 
+        (setq exwm-randr-workspace-monitor-plist '(1 "DP-0" 2 "DP-0" 3 "DP-0" 7 "DP-4" 8 "DP-4" 9
+                                                          "DP-4"))
+        ;;exwm-workspace-warp-cursor t)
+        ;;mouse-autoselect-window t)
 
 
-  ;; Load the system tray before exwm-init
-  ;; (require 'exwm-systemtray)
-  ;; (exwm-systemtray-enable)
+        ;; Load the system tray before exwm-init
+        ;; (require 'exwm-systemtray)
+        ;; (exwm-systemtray-enable)
 
-  ;; Allow buffers on all workspaces
-  ;; (setq exwm-workspace-show-all-buffers 1)
-  ;; (setq exwm-layout-show-all-buffers t)
+        ;; Allow buffers on all workspaces
+        ;; (setq exwm-workspace-show-all-buffers 1)
+        ;; (setq exwm-layout-show-all-buffers t)
 
-  ;; These keys should always pass through to Emacs
-  (setq exwm-input-prefix-keys
-    '(?\C-x
-      ?\C-u
-      ?\C-h
-      ?\M-x
-      ?\M-`
-      ?\M-&
-      ?\M-:
-      ?\C-\M-j  ;; Buffer list
-      ?\C-\ ))  ;; Ctrl+Space
+        ;; These keys should always pass through to Emacs
+        (setq exwm-input-prefix-keys '(?\C-x ?\C-u ?\C-h ?\M-x ?\M-`?\M-& ?\M-: ?\C-\M-j ;; Buffer list
+                                              ?\C-\ )) ;; Ctrl+Space
 
-  ;; Ctrl+Q will enable the next key to be sent directly
-  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+        ;; Ctrl+Q will enable the next key to be sent directly
+        (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
 
-  ;; Set up global key bindings.  These always work, no matter the input state!
-  ;; Keep in mind that changing this list after EXWM initializes has no effect.
-  (setq exwm-input-global-keys
-	`(
-	  ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
-	  ([?\s-r] . exwm-reset)
+        ;; Set up global key bindings.  These always work, no matter the input state!
+        ;; Keep in mind that changing this list after EXWM initializes has no effect.
+        (setq exwm-input-global-keys `(
+                                              ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
+                                              ([?\s-r] . exwm-reset)
 
-	  ;; Move between windows
-	  ([s-left] . windmove-left)
-	  ([s-right] . windmove-right)
-	  ([s-up] . windmove-up)
-	  ([s-down] . windmove-down)
-	  ([C-s-b] . windmove-left)
-	  ([C-s-f] . windmove-right)
-	  ([C-s-p] . windmove-up)
-	  ([C-s-n] . windmove-down)
+                                              ;; Move between windows
+                                              ([s-left] . windmove-left) 
+                                              ([s-right] . windmove-right) 
+                                              ([s-up] . windmove-up) 
+                                              ([s-down] . windmove-down) 
+                                              ([C-s-b] . windmove-left) 
+                                              ([C-s-f] . windmove-right) 
+                                              ([C-s-p] . windmove-up) 
+                                              ([C-s-n] . windmove-down)
 
-	  ;; Launch applications via shell command
-	  ([?\s-&] . (lambda (command)
-		       (interactive (list (read-shell-command "$ ")))
-		       (start-process-shell-command command nil command)))
+                                              ;; Launch applications via shell command
+                                              ([?\s-&] . (lambda (command) 
+                                                                 (interactive (list
+                                                                                      (read-shell-command
+                                                                                              "$
+"))) 
+                                                                 (start-process-shell-command
+                                                                         command nil command)))
 
-	  ;; Switch workspace
-	  ([?\s-w] . exwm-workspace-switch)
-	  ([?\s-`] . (lambda () (interactive) (exwm-workspace-switch-create 0)))
+                                              ;; Switch workspace
+                                              ([?\s-w] . exwm-workspace-switch) 
+                                              ([?\s-`] . (lambda () 
+                                                                 (interactive) 
+                                                                 (exwm-workspace-switch-create 0)))
 
-	  ;; Toggle char line mode
-	  ([?\s-k] . exwm-input-toggle-keyboard)
-	  ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
-	  ,@(mapcar (lambda (i)
-		      `(,(kbd (format "s-%d" i)) .
-			(lambda ()
-			  (interactive)
-			  (exwm-workspace-switch-create ,i))))
-		    (number-sequence 0 9))))
+                                              ;; Toggle char line mode
+                                              ([?\s-k] . exwm-input-toggle-keyboard)
+                                              ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+                                              ,@(mapcar (lambda (i) 
+                                                                `(,(kbd (format "s-%d" i)) . (lambda
+                                                                                                     () 
+                                                                                                     (interactive) 
+                                                                                                     (exwm-workspace-switch-create
+                                                                                                             ,i)))) 
+                                                        (number-sequence 0 9)))) 
+        (exwm-input-set-key (kbd "s-SPC") 'counsel-linux-app) 
+        (exwm-input-set-key (kbd "s-f") 'exwm-layout-toggle-fullscreen)
 
-  (exwm-input-set-key (kbd "s-SPC") 'counsel-linux-app)
-  (exwm-input-set-key (kbd "s-f") 'exwm-layout-toggle-fullscreen)
-  (exwm-input-set-key (kbd "s-l") (lambda() (interactive) (clover/exwm-lock-screen)))
-
-  (exwm-enable)
-)
+        (exwm-input-set-key (kbd "s-l l") 'clover/exwm-lock-screen)
+        (exwm-enable))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -302,3 +302,16 @@ _q_: decrease    _k_: down
         ("l" windmove-right))
 
 (general-define-key "A-z" 'hydra-windows/body)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(elisp-format yaml-mode which-key use-package terraform-mode rainbow-delimiters powershell org-bullets ob-powershell multiple-cursors magit lsp-ui lsp-treemacs lsp-ivy keycast ivy-rich helpful general exwm evil-nerd-commenter eshell-git-prompt doom-themes doom-modeline counsel-projectile company-box command-log-mode all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
