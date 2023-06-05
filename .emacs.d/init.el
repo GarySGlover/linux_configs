5a2a5298b8cb1752e8b5f1a29020ae555f2d4c8f
Gary Glover <light.bed5489@fourleafclover.uk>
2023-06-05 16:04:43 +0100

    Auto tangle config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,21 +1,21 @@
 ;; Initialize package sources
 (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/") 
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
                                 ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 
 ;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package) 
+(unless (package-installed-p 'use-package)
         (package-install 'use-package))
 
 ;; (require 'use-package)
 (setq use-package-always-ensure t)
 
 (require 'server)
-(unless (server-running-p) 
+(unless (server-running-p)
         (server-start))
 
 (setq disabled-command-function nil)
@@ -37,43 +37,43 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil 
-        :font "Fira Code Retina" 
+(set-face-attribute 'default nil
+        :font "Fira Code Retina"
         :height 100)
 
-(use-package 
+(use-package
         all-the-icons)
 ;; :init
 ;; (all-the-icons-install-fonts t)
 
-(use-package 
-        doom-modeline 
-        :ensure t 
-        :init (doom-modeline-mode 1) 
+(use-package
+        doom-modeline
+        :ensure t
+        :init (doom-modeline-mode 1)
         :custom ((doom-modeline-height 15)))
 
-(use-package 
-        doom-themes 
+(use-package
+        doom-themes
         :init (load-theme 'doom-dracula t))
 
 (setq backup-directory-alist '((".*" . "~/.emacs.d/emacs_backup")))
 (setq create-lockfiles nil)
 (setq auto-save-file-name-transforms `((".*" "~/.emacs.d/emacs_backup" t)))
 
-(defun efs/configure-eshell () 
+(defun efs/configure-eshell () P
         (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
         (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
         (setq eshell-history-size 10000 eshell-buffer-maximum-lines 10000 eshell-hist-ignoredups t
                 eshell-scroll-to-bottom-on-input t)
         (setq eshell-visual-commands (append eshell-visual-commands '("bash" "zsh" "bluetuith"))))
 
-(use-package 
+(use-package
         eshell-git-prompt)
 
-(use-package 
-        eshell 
+(use-package
+        eshell
         :hook (eshell-first-time-mode . efs/configure-eshell)
-        :config (with-eval-after-load 'esh-opt 
+        :config (with-eval-after-load 'esh-opt
                         (setq eshell-destroy-buffer-when-process-dies t))
         (eshell-git-prompt-use-theme 'powerline))
 
@@ -93,23 +93,23 @@
 
 (use-package whole-line-or-region)
 
-(use-package 
-        which-key 
-        :init (which-key-mode) 
-        :diminish which-key-mode 
+(use-package
+        which-key
+        :init (which-key-mode)
+        :diminish which-key-mode
         :config)
 
-(use-package 
-        helpful 
-        :custom (counsel-describe-function-function #'helpful-callable) 
-        (counsel-describe-variable-function #'helpful-variable) 
-        :bind ([remap describe-function] . counsel-describe-function) 
-        ([remap describe-command] . helpful-command) 
-        ([remap describe-variable] . counsel-describe-variable) 
+(use-package
+        helpful
+        :custom (counsel-describe-function-function #'helpful-callable)
+        (counsel-describe-variable-function #'helpful-variable)
+        :bind ([remap describe-function] . counsel-describe-function)
+        ([remap describe-command] . helpful-command)
+        ([remap describe-variable] . counsel-describe-variable)
         ([remap describe-key] . helpful-key))
 
-(use-package 
-        keycast 
+(use-package
+        keycast
         :init (keycast-tab-bar-mode))
 
 (setq-default indent-tabs-mode nil)
@@ -118,9 +118,17 @@
 
 (org-babel-do-load-languages 'org-babel-load-languages '((shell . t)))
 
-(use-package 
-        org-bullets 
-        :after org 
+(defun org-babel-tangle-config ()
+    (when (string-equal (buffer-file-name)
+              (expand-file-name "~/linux_configs/emacs_config.org"))
+        (let ((org-config-babel-evaluate nil))
+            (org-babel-tangle))))
+(add-hook 'org-mode-hook (lambda ()
+                             (add-hook 'after-save-hook #'org-babel-tangle-config)))
+
+(use-package
+        org-bullets
+        :after org
         :hook (org-mode . org-bullets-mode))
 
 (require 'org-tempo)
@@ -130,8 +138,8 @@
 
 (setq org-src-window-setup 'current-window)
 
-(use-package 
-        rainbow-delimiters 
+(use-package
+        rainbow-delimiters
         :hook (prog-mode . rainbow-delimiters-mode))
 
 (setq-default fill-column 80)
@@ -141,12 +149,12 @@
 (global-display-line-numbers-mode t)
 
 ;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook term-mode-hook shell-mode-hook treemacs-mode-hook eshell-mode-hook)) 
-        (add-hook mode (lambda () 
+(dolist (mode '(org-mode-hook term-mode-hook shell-mode-hook treemacs-mode-hook eshell-mode-hook))
+        (add-hook mode (lambda ()
                                (display-line-numbers-mode 0))))
 
-(use-package 
-        evil-nerd-commenter 
+(use-package
+        evil-nerd-commenter
         :bind ("C-;" . evilnc-comment-or-uncomment-lines))
 
 (use-package highlight-indent-guides
@@ -165,94 +173,97 @@
     :init (setq whitespace-style '(face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark newline-mark))
     :bind (("C-c w" . whitespace-mode)))
 
+(use-package ws-butler
+    :hook prog-mode org-mode)
+
 (defhydra hydra-windows (global-map "s-j" :hint nil)
-        ("e" (clover/font-size-increase 5)) 
-        ("q" (clover/font-size-decrease 5)) 
-        ("i" windmove-up) 
-        ("k" windmove-down) 
-        ("j" windmove-left) 
+        ("e" (clover/font-size-increase 5))
+        ("q" (clover/font-size-decrease 5))
+        ("i" windmove-up)
+        ("k" windmove-down)
+        ("j" windmove-left)
         ("l" windmove-right))
 (general-define-key "M-o" 'other-window)
 
 (use-package resize-window
   :bind (("C-c r" . resize-window)))
 
-(defun display-buffer-from-compilation-p (_buffer-name _action) 
-  (unless current-prefix-arg (with-current-buffer (window-buffer) 
+(defun display-buffer-from-compilation-p (_buffer-name _action)
+  (unless current-prefix-arg (with-current-buffer (window-buffer)
                                (derived-mode-p 'compilation-mode))))
 
 (push '(display-buffer-from-compilation-p display-buffer-same-window (inhibit-same-window . nil))
   display-buffer-alist)
 
-(use-package 
-        counsel 
-        :bind (("M-x" . counsel-M-x) 
-                      ("C-x b" . counsel-switch-buffer) 
-                      ("C-x C-f" . counsel-find-file) 
-                      :map minibuffer-local-map ("C-r" . 'counsel-minibuffer-history)) 
-        :custom (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only) 
-        :config (setq ivy-initial-inputs-alist nil) 
+(use-package
+        counsel
+        :bind (("M-x" . counsel-M-x)
+                      ("C-x b" . counsel-switch-buffer)
+                      ("C-x C-f" . counsel-find-file)
+                      :map minibuffer-local-map ("C-r" . 'counsel-minibuffer-history))
+        :custom (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+        :config (setq ivy-initial-inputs-alist nil)
         (setq counsel-switch-buffer-preview-virtual-buffers nil))
 
-(use-package 
-        swiper 
+(use-package
+        swiper
         :ensure t)
 
-(use-package 
-        ivy 
+(use-package
+        ivy
 
-        :diminish 
-        :bind (("C-s" . swiper) :map ivy-minibuffer-map ("TAB" . ivy-alt-done) 
-                      ("C-l" . ivy-alt-done) 
-                      ("C-j" . ivy-next-line) 
-                      ("C-k" . ivy-previous-line) 
-                      :map ivy-switch-buffer-map ("C-k" . ivy-previous-line) 
-                      ("C-l" . ivy-done) 
-                      ("C-d" . ivy-switch-buffer-kill) 
-                      :map ivy-reverse-i-search-map ("C-k" . ivy-previous-line) 
-                      ("C-d" . ivy-reverse-i-search-kill)) 
+        :diminish
+        :bind (("C-s" . swiper) :map ivy-minibuffer-map ("TAB" . ivy-alt-done)
+                      ("C-l" . ivy-alt-done)
+                      ("C-j" . ivy-next-line)
+                      ("C-k" . ivy-previous-line)
+                      :map ivy-switch-buffer-map ("C-k" . ivy-previous-line)
+                      ("C-l" . ivy-done)
+                      ("C-d" . ivy-switch-buffer-kill)
+                      :map ivy-reverse-i-search-map ("C-k" . ivy-previous-line)
+                      ("C-d" . ivy-reverse-i-search-kill))
         :config (ivy-mode 1))
 
-(use-package 
-        ivy-rich 
+(use-package
+        ivy-rich
         :init (ivy-rich-mode 1))
 
-(use-package 
+(use-package
         treemacs)
 (general-define-key "C-c d" 'treemacs-select-window)
 
-(use-package 
-        projectile 
-        :diminish projectile-mode 
-        :config (projectile-mode) 
-        :bind-keymap ("C-c p" . projectile-command-map) 
+(use-package
+        projectile
+        :diminish projectile-mode
+        :config (projectile-mode)
+        :bind-keymap ("C-c p" . projectile-command-map)
         :init (setq projectile-switch-project-action #'projectile-dired))
 
 ;; Projectile Counsel
-(use-package 
-        counsel-projectile 
-        :after projectile 
+(use-package
+        counsel-projectile
+        :after projectile
         :config (counsel-projectile-mode 1))
 
-(use-package 
-        magit 
-        :commands (magit-status magit-get-current-branch) 
-        :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1) 
+(use-package
+        magit
+        :commands (magit-status magit-get-current-branch)
+        :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
         :bind ("C-c g" . magit-status))
 
-(defun efs/lsp-mode-setup () 
-        (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols)) 
+(defun efs/lsp-mode-setup ()
+        (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
         (lsp-headerline-breadcrumb-mode))
 
-(use-package 
-        lsp-mode 
-        :commands (lsp lsp-deferred) 
-        :init (setq lsp-keymap-prefix "C-c l") 
-        :config (lsp-enable-which-key-integration t) 
+(use-package
+        lsp-mode
+        :commands (lsp lsp-deferred)
+        :init (setq lsp-keymap-prefix "C-c l")
+        :config (lsp-enable-which-key-integration t)
         :hook (lsp-mode . efs/lsp-mode-setup))
 
-(use-package 
-        lsp-ui 
+(use-package
+        lsp-ui
         :hook (lsp-mode . lsp-ui-mode))
 
 (use-package lsp-treemacs
@@ -260,73 +271,86 @@
 
 (use-package lsp-ivy)
 
-(use-package 
-        company 
-        :after lsp-mode 
-        :hook (prog-mode . company-mode) 
+(use-package
+        company
+        :after lsp-mode
+        :hook (prog-mode . company-mode)
         :bind (:map company-active-map
-                      ("<tab>" . company-complete-selection)) 
-        (:map lsp-mode-map 
-                ("<tab>" . company-indent-or-complete-common)) 
-        :custom (company-minimum-prefix-length 1) 
+                      ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+                ("<tab>" . company-indent-or-complete-common))
+        :custom (company-minimum-prefix-length 1)
         (company-idle-delay 0.0))
 
-(use-package 
-        company-box 
+(use-package
+        company-box
         :hook (company-mode . company-box-mode))
 
-(use-package 
-        elisp-format 
+(use-package
+        elisp-format
         :bind (:map emacs-lisp-mode-map
                       ("C-c f" . elisp-format-buffer)))
 
-(use-package 
+(use-package
         ob-powershell)
-(use-package 
+(use-package
         powershell)
 
-(use-package 
-        terraform-mode 
+(use-package
+        terraform-mode
         :hook (terraform-mode . lsp-deferred))
 
-(use-package 
-        yaml-mode 
+(use-package
+        yaml-mode
         :hook (yaml-mode . lsp-deferred))
 
-(defun clover/set-frame-font-size (SIZE) 
-        (interactive "nFont Size: ") 
-        (set-face-attribute 'default (selected-frame) 
+(defun clover/set-frame-font-size (SIZE)
+        (interactive "nFont Size: ")
+        (set-face-attribute 'default (selected-frame)
                 :height SIZE))
 
-(defun clover/font-size-increase (BY) 
-        (interactive "nFont Size Increase Amount: ") 
-        (let ((height (face-attribute 'default 
-                              :height (selected-frame)))) 
+(defun clover/font-size-increase (BY)
+        (interactive "nFont Size Increase Amount: ")
+        (let ((height (face-attribute 'default
+                              :height (selected-frame))))
                 (clover/set-frame-font-size (+ BY height))))
 
-(defun clover/font-size-decrease (BY) 
-        (interactive "nFont Size Decrease Amount: ") 
+(defun clover/font-size-decrease (BY)
+        (interactive "nFont Size Decrease Amount: ")
         (clover/font-size-increase (- BY)))
 
-(defun clover-counsel-switch-buffer (regex-list) 
-        (let ((ivy-ignore-buffers (append ivy-ignore-buffers regex-list))) 
+(defun clover-counsel-switch-buffer (regex-list)
+        (let ((ivy-ignore-buffers (append ivy-ignore-buffers regex-list)))
                 (ivy-switch-buffer)))
 
-(defun clover-show-only-firefox-buffers () 
-        (interactive) 
+(defun clover-show-only-firefox-buffers ()
+        (interactive)
         (clover-ignore-star-and-buffers '("^[^F][^i][^r]")))
 
-(defun clover-show-only-brave-buffers () 
-        (interactive) 
+(defun clover-show-only-brave-buffers ()
+        (interactive)
         (clover-ignore-star-and-buffers '("^[^B][^r][^a][^v][^e]")))
 
-(defun clover-ignore-star-buffers () 
-        "ignore everything starting with a star along with whatever ivy's defaults are" 
-        (interactive) 
+(defun clover-ignore-star-buffers ()
+        "ignore everything starting with a star along with whatever ivy's defaults are"
+        (interactive)
         (clover-counsel-switch-buffer (append ivy-ignore-buffers '("^\*"))))
 
-(defun clover-ignore-star-and-buffers (regex-list) 
-        (interactive) 
+(defun clover-ignore-star-and-buffers (regex-list)
+        (interactive)
         (clover-counsel-switch-buffer (append ivy-ignore-buffers '("^\*") regex-list)))
 
 (general-define-key "C-x b" 'clover-ignore-star-buffers)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+      '(js-auto-beautify web-beautify ws-butler whole-line-or-region which-key use-package terraform-mode resize-window rainbow-delimiters powershell org-bullets ob-powershell nov multiple-cursors magit lsp-ui lsp-treemacs lsp-ivy kubel kubedoc keycast kele ivy-rich highlight-indent-guides helpful general exwm evil-nerd-commenter eshell-git-prompt elisp-format dtrt-indent doom-themes doom-modeline docker counsel-projectile company-box command-log-mode all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
