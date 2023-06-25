f293218c7e223bac303a6d03c40e0557e8ea6367
Gary Glover <light.bed5489@fourleafclover.uk>
2023-06-25 09:19:35 +0100

    Auto update packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,6 +14,15 @@
 ;; (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+    :custom
+    (auto-package-update-interval 1)
+    (auto-package-prompt-before-update t)
+    (auto-package-update-hide-results t)
+    :config
+    (auto-package-update-maybe)
+    (auto-package-update-at-time 10:00))
+
 (require 'server)
 (unless (server-running-p)
         (server-start))
